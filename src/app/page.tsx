55d4c38d8f699b758bee31025f6bb71a4ede245b
Andrea Lin <andrea.lin3110@gmail.com>
2023-12-24 23:40:32 +0100

    ✨ Add `.prettierrc`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import { FunctionComponent } from "react"
+
+const RootPage: FunctionComponent = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello World!
     </main>
   )
 }
+
+export default RootPage
