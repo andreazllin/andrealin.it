2578e9dc9b2b8c9aa7aebbe237dcede414cb6837
Andrea Lin <andrea.lin3110@gmail.com>
2024-09-21 21:58:17 +0200

    ✨ Update `projects.ts`

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -9,25 +9,24 @@ export const projects: Array<ProjectProps> = [
     url: "https://polarity.dev"
   },
   {
-    name: "XDEX",
-    description: "A cryptocurrency exchange with a focus on privacy and security.",
-    work: "A mobile app for iOS and Android, built with React Native and custom native modules.",
-    image: "/assets/images/projects/xdex.svg",
-    url: "https://xdex.ch"
+    name: "Sellix",
+    description: "An e-commerce solution built for creating and managing an online store.",
+    work: "Sellix Wallet, a browser wallet with support for multiple cryptocurrencies made to work inside an iframe.",
+    image: "/assets/images/projects/sellix.webp",
+    url: "https://sellix.io"
   },
+  // {
+  //   name: "XDEX",
+  //   description: "A cryptocurrency exchange with a focus on privacy and security.",
+  //   work: "A mobile app for iOS and Android, built with React Native and custom native modules.",
+  //   image: "/assets/images/projects/xdex.svg",
+  //   url: "https://xdex.ch"
+  // },
   {
     name: "Holyart",
     description: "The first Italian e-commerce dedicated to sacred art and religious items.",
     work: "Various projects including the mobile app, the website and some scripts.",
     image: "/assets/images/projects/holyart.png",
     url: "https://holyart.it"
-  },
-
-  {
-    name: "Sellix",
-    description: "An e-commerce solution built for creating and managing an online store.",
-    work: "Sellix Wallet, a browser wallet with support for multiple cryptocurrencies made to work inside an iframe.",
-    image: "/assets/images/projects/sellix.webp",
-    url: "https://sellix.io"
   }
 ]
