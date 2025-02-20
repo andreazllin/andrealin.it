ed76ed9097ac6d9fa41e6591b02ac39f9ad2ab30
Andrea <andrea.lin3110@gmail.com>
2025-02-21 00:49:38 +0100

    Initial Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,27 @@
+import "./globals.css"
 import { FunctionComponent, PropsWithChildren } from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "./globals.css"
+import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"]
 })
 
-const RootLayout: FunctionComponent<PropsWithChildren> = ({
-  children
-}) => {
+const RootLayout: FunctionComponent<PropsWithChildren> = ({ children }) => {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(inter.className, "bg-slate-50 dark:bg-slate-900")}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Squares from "@/components/react-bits/Squares/Squares"
+import { ThemeToggle } from "@/components/theme-provider"
 import { FunctionComponent } from "react"
 
 const Home: FunctionComponent = () => {
-  return <div></div>
+  return (
+    <div className="mx-auto min-h-screen border-slate-200 md:max-w-10/12 md:border-x xl:max-w-8/12 dark:border-slate-700">
+      <section className="h-[256px] bg-slate-900">
+        <Squares
+          direction="diagonal"
+          speed={0.25}
+          squareSize={48}
+          borderColor={"#454C56"}
+          hoverFillColor={"#1B1F23"}
+        />
+      </section>
+      <ThemeToggle />
+    </div>
+  )
 }
 
 export default Home
