889cfd969175122b56693adf3a4e49480d11aff3
Andrea <andrea.lin3110@gmail.com>
2025-02-25 17:22:37 +0100

    Fix `about-me`

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -26,7 +26,7 @@ export const AboutMe: FunctionComponent = () => {
         )}
       >
         I&apos;m a passionate Web developer with over 3 years of professional
-        experience, capable of working in a team or managing a small team.
+        experience, capable of working in a team.
       </p>
       <br />
       <p
@@ -41,6 +41,19 @@ export const AboutMe: FunctionComponent = () => {
         With a proven track record in developing frontend and backend solutions
         for Web and Mobile platforms, mainly with React (and React Native) using
         TypeScript.
+      </p>
+      <br />
+      <p
+        className={typography(
+          {
+            size: "text-sm",
+            suggestedMaxWidth: true
+          },
+          "text-secondary"
+        )}
+      >
+        Progressively transitioned from a hands-on full-stack development to a
+        leadership position and mentoring small development teams.
       </p>
     </div>
   )
